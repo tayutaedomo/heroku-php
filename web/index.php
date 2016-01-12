3d5e5faffe09e73c95a542c5e38eb1739ea9dbbc
tayutaedomo <tayutaedomo@gmail.com>
2016-01-12 23:28:17 +0900

    Add index.php

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8" />
+    <title>phpinfo</title>
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <!-- Compressed CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.css">
+    <!-- Compressed JavaScript -->
+    <script src="https://cdn.jsdelivr.net/foundation/6.1.1/foundation.min.js"></script>
+</head>
+<body>
+<div class="row">
+    <div class="small-12">
+        <div class="success callout">
+            <p><?php print 'Hello, world!'; ?></p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
